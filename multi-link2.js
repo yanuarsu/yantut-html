7b1903188f576909f1a2488841c3dbada8a88092
yanuarsu <31426236+yanuarsu@users.noreply.github.com>
2017-08-30 09:26:07 +0700

    Update multi-link2.js

--- multi-link2.js
+++ multi-link2.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="how-to-read-forex-chart-properly_29.html"}
-if(x=="2"){xxx="google-drive-cloud_29.html"}
-if(x=="3"){xxx="auto-insurance-qoutes-2015.html"}
-if(x=="4"){xxx="ways-to-get-cheapest-health-insuranc.html"}
-if(x=="5"){xxx="stress-eating-helps.html"}a_to_ve[i].href="https://yan-link.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2017/08/how-to-read-forex-chart-properly_29.html"}
+if(x=="2"){xxx="/2017/08/google-drive-cloud_29.html"}
+if(x=="3"){xxx="/2017/08/auto-insurance-qoutes-2015.html"}
+if(x=="4"){xxx="/2017/08/ways-to-get-cheapest-health-insuranc.html"}
+if(x=="5"){xxx="/2017/08/stress-eating-helps.html"}a_to_ve[i].href="https://yan-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
